a142704f8bfa1091163aad8ab47e14d483fd1a2a
sinspired <ggmomo@gmail.com>
2024-08-08 19:06:12 +0800

    update creat_release.yml

--- main.go
+++ main.go
@@ -508,6 +508,7 @@ func autoNetworkDetection() bool {
 
 // checkProxyEnabled 检测是否开启系统代理服务器
 func checkProxyEnabled() bool {
+	// +build windows
 	k, err := registry.OpenKey(registry.CURRENT_USER, `Software\Microsoft\Windows\CurrentVersion\Internet Settings`, registry.QUERY_VALUE)
 	if err != nil {
 		fmt.Println("无法打开注册表键:", err)
